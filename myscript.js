9a6756468834fb74f42593e523753dce41010c1a
Brandon Work <blg1209@gmail.com>
2022-07-01 12:14:10 -0500

    better version of previous - grid is displaying block though and not 100% working

--- myscript.js
+++ myscript.js
@@ -1,6 +1,7 @@
 // select the 600 x 600 grid-container
 const container = document.querySelector('#grid-container');
 
+
 // this corresponds to the 16x16 in the #grid-container
 let defaultSize = 256    
 function makeSquares() {
@@ -11,16 +12,32 @@ function makeSquares() {
             }                
         }
 
+// access the range on the sidebar when it's selected and call function that returns the value
 let slider = document.getElementById('slider').oninput = function(){
     displaySliderValue();
 }
-
 function displaySliderValue(){
     let val = document.getElementById('slider').value;
-    let updateText = document.getElementById('slider-value').textContent;
-    
-
-
-    console.log(updateText.textContent = (val));
+    updateSquares(val);
+    console.log(val);
 }
+
+// function called when slider is adjusted. takes in slider value, updates grid and CSS
+function updateSquares(num){
+    let col = num;
+    let row = num;
+    let squared = col * row;
+
+    container.style.setProperty('grid-template-columns', 'repeat(${squared},1fr)');
+    container.style.setProperty('grid-template-rows', 'repeat(${squared},1fr)');
+
+    for(let i=0; i<squared; i++){
+        let square = document.createElement('div');
+        square.className = "row";
+        container.appendChild(square)
+    }
+}      
+
+
 makeSquares();
+
